080ad849a716600d75d118949f7feaee885df97c
sunaga <sunaga@gmail.com>
2020-01-17 21:27:52 +0700

    rails gコマンドでjavascript,css,を自動生成しないように設定

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,14 @@ module IGclone
     #timezoneを日本時間に設定
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    
+
+
+    # css,html,javascripを自動生成しない設定
+    config.generators do |g|
+    g.assets false
+    g.helper false
+  end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
