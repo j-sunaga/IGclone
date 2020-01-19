62bac0aa95183344c1e344c6abe0439c071ad5af
sunaga <sunaga@gmail.com>
2020-01-19 14:15:04 +0700

    Usersコントローラ/VIewファイル/ルーティングの作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def update
+  end
+
+end
