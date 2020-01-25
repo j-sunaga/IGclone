30579aa5630107bb322ddf5ebb73ac987dcb8494
sunaga <sunaga@gmail.com>
2020-01-25 21:50:55 +0700

    picturesへのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  root to: 'sessions#new'
+
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
+
   resources :sessions, only: [:new, :create, :destroy]
   resource :user
 
