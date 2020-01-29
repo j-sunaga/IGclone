609fcc794adc4b37d84f94edd1193b7ff54982de
sunaga <sunaga@gmail.com>
2020-01-29 22:01:09 +0700

    アソシエーションの追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,10 @@
 class Picture < ApplicationRecord
+
+  #アソシエーション
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
+
   validates :title, presence: true, length: { in: 1..20 }
   validates :content, presence: true, length: { in: 1..200 }
   mount_uploader :image, ImageUploader
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 
   #アソシエーション
   has_many :pictures
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_pictures, through: :favorites, source: :picture
 
   #Validationの追加
   validates :name, presence: true, length: {maximum: 30}
