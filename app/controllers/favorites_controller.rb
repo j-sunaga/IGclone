c27b338b9c695379485ef55627b2e97cb4eda341
sunaga <sunaga@gmail.com>
2020-02-02 12:17:01 +0700

    favoriteコントローラの修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,12 @@ class FavoritesController < ApplicationController
 
   def create
    favorite = current_user.favorites.create(picture_id: params[:picture_id])
-   redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんの写真をお気に入り登録しました"
+   redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんの写真をお気に入り登録しました"
  end
 
  def destroy
    favorite = current_user.favorites.find_by(id: params[:id]).destroy
-   redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんの写真をお気に入り解除しました"
+   redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんの写真をお気に入り解除しました"
  end
 
 end
