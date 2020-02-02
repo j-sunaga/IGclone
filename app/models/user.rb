4479484519884d209f3c28638b95cc66581a955c
sunaga <sunaga@gmail.com>
2020-02-02 12:04:12 +0700

    throughメソッドのエラーが発生するため、メソッドの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   #アソシエーション
   has_many :pictures
   has_many :favorites, dependent: :destroy
-  has_many :favorite_pictures, through: :favorites, source: :picture
 
   #Validationの追加
   validates :name, presence: true, length: {maximum: 30}
