4a1efa9c6489e77c347f5c85bd0702a37b20af27
sunaga <sunaga@gmail.com>
2020-02-09 18:15:14 +0700

    mailerの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,19 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  #Mailer
+  config.action_mailer.default_url_options = { host: 'limitless-taiga-67145.herokuapp.com' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    address: "smtp.SendGrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
